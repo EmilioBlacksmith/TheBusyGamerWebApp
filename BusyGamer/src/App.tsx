be15ab39bc4255f4d1ea439fe6810de3026e2e80
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-11-26 17:50:02 -0600

    create header module

--- BusyGamer/src/App.tsx
+++ BusyGamer/src/App.tsx
@@ -1,28 +1,10 @@
+import Header from "./Components/Header";
+
 function App() {
   return (
     <div className="flex flex-col min-h-screen min-w-full items-center bg-app-main text-white font-sans">
-      <div className="w-5/6 h-24 rounded-b-xl bg-app-secondary drop-shadow-md sticky top-0 flex flex-row items-center justify-between p-8">
-        <div className="text-4xl font-bold">
-          <a
-            href="/"
-            className="hover:text-app-complementary hover:cursor-pointer"
-          >
-            The Busy Gamer
-          </a>
-          <a className="text-xs font-thin text-app-grey pl-8">
-            I can only play 2 hours... :(
-          </a>
-        </div>
-        <div className="flex flex-row gap-3">
-          <div className="bg-app-complementary h-10 w-10 rounded-md items-center justify-center flex text-xl drop-shadow-md hover:cursor-pointer hover:scale-105">
-            
-          </div>
-          <div className="bg-app-complementary h-10 w-10 rounded-md items-center pl-3 flex text-lg drop-shadow-md hover:cursor-pointer hover:scale-105">
-            
-          </div>
-        </div>
-      </div>
-      <div id="content">Nigga</div>
+      <Header />
+      <p>Welcome My Nigga</p>
     </div>
   );
 }
