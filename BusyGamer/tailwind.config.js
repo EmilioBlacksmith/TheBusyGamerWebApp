29aac2cf4bb02733baae4cfefd9caf16065ea671
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-11-26 17:37:37 -0600

    header style done || install tailwind and apply theme extensions

--- BusyGamer/tailwind.config.js
+++ BusyGamer/tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "app-main": "#2D2D2D",
+      "app-secondary": "#242424",
+      "app-secondary-dark": "#171717",
+      "app-complementary": "#0063AA",
+      "app-grey": "#696969",
+      white: "#FFF",
+    },
+    fontFamily: {
+      sans: ["JetBrains"],
+    },
     extend: {},
   },
   plugins: [],
