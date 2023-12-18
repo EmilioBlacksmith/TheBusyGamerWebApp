b3ede65e0c061699cc92679f0085c70717e05ec2
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-12-18 11:24:06 -0600

    onsubmit done lacks search functionality

--- BusyGamerWebApp/src/Components/SearchBox.jsx
+++ BusyGamerWebApp/src/Components/SearchBox.jsx
@@ -1,7 +1,19 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
 
 export default function SearchBox({ open, onClose }) {
+	const [value, setValue] = useState("");
+
+	const onChange = (e) => {
+		setValue(e.target.value);
+	};
+
+	const onSearch = (event) => {
+		// Fetch Data from API
+		event.preventDefault();
+		console.log(value);
+	};
+
 	if (!open) return null;
 
 	return ReactDom.createPortal(
@@ -14,15 +26,20 @@ export default function SearchBox({ open, onClose }) {
 					className="w-4/5 h-14 bg-app-main z-20 rounded-xl drop-shadow-3xl"
 					onClick={(e) => e.stopPropagation()}
 				>
-					<form>
+					<form onSubmit={onSearch}>
 						<input
 							type="text"
 							className="w-full h-14 rounded-xl bg-app-main font-extrabold text-2xl text-white placeholder:text-app-grey placeholder:text-2xl placeholder:font-extrabold pl-6 outline-none focus:border-b-[1px] focus:border-opacity-50 transition-all"
 							placeholder="WHAT ARE WE PLAYING NEXT?"
+							value={value}
+							onChange={onChange}
 						/>
-						<div className="text-app-grey text-2xl relative float-right right-6 -top-11 pointer-events-none">
-							
-						</div>
+						<button
+							className="text-app-grey text-2xl relative float-right right-6 -top-11 cursor-pointer"
+							type="submit"
+						>
+							 SEARCH
+						</button>
 					</form>
 				</div>
 			</div>
